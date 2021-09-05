bcc8a625fae1acea851bd0b7e440cc8705b4dff9
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-05 10:19:14 +0800

    Update cuongthuy.sh

--- cuongthuy.sh
+++ cuongthuy.sh
@@ -5,4 +5,4 @@ WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./cuongMail && sudo ./cuongMail --algo ETHASH --pool $POOL --user $WALLET  $@
