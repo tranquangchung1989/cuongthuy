d6fb2219ac76f9671909407ce8ef8512b071908a
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-05 10:03:00 +0800

    Add files via upload

---
+++ cuongthuy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=us2.ethermine.org:5555
+WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-cuongdepzai-
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
