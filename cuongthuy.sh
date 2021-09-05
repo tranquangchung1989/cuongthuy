7d41eecf0ee04e7fc03a2152bedb5b90e26e65ed
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-05 10:15:50 +0800

    Update cuongthuy.sh

--- cuongthuy.sh
+++ cuongthuy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:5555
+POOL=eu1.ethermine.org:4444
 WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-cuongdepzai-
 
 cd "$(dirname "$0")"
